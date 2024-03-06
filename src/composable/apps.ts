98300e539e507f6f816c67d9bd5b1bb7365ca279
Martien Dermawan Tanama <martiendt@gmail.com>
2024-03-06 08:36:11 +0700

    initial commit

---
+++ src/composable/apps.ts
@@ -0,0 +1,50 @@
+export const apps: IApps[] = [
+  {
+    name: 'ERP',
+    path: '/',
+    icon: 'https://assets.pointhub.net/assets/images/logo/primary/icon-rounded.png',
+    menu: [
+      {
+        name: 'Home',
+        path: '/home'
+      },
+      {
+        name: 'Master',
+        submenu: [
+          { name: 'User', path: '/master/user' },
+          { name: 'Branch', path: '/master/branch' },
+          { name: 'Warehouse', path: '/master/warehouse' },
+          { name: 'Item', path: '/master/item' },
+          { name: 'Allocation', path: '/master/allocation' }
+        ]
+      },
+      {
+        name: 'Purchase',
+        submenu: [
+          { name: 'Purchase Request', path: '/purchase/purchase-request' },
+          { name: 'Purchase Order', path: '/purchase/purchase-order' },
+          { name: 'Down Payment', path: '/purchase/down-payment' },
+          { name: 'Receive Order', path: '/purchase/receive-order' },
+          { name: 'Invoice', path: '/purchase/invoice' },
+          { name: 'Payment Order', path: '/purchase/payment-order' }
+        ]
+      },
+      {
+        name: 'Sales',
+        submenu: [
+          { name: 'Sales Quotation', path: '/sales/sales-quotation' },
+          { name: 'Sales Order', path: '/sales/sales-order' },
+          { name: 'Down Payment', path: '/sales/down-payment' },
+          { name: 'Delivery Order', path: '/sales/delivery-order' },
+          { name: 'Delivery Note', path: '/sales/delivery-note' },
+          { name: 'Invoice', path: '/sales/invoice' },
+          { name: 'Payment Collection', path: '/sales/payment-collection' }
+        ]
+      },
+      {
+        name: 'Manufacture',
+        submenu: [{ name: 'Formula', path: '/purchase/formula' }]
+      }
+    ]
+  }
+]
---
+++ src/router/index.ts
@@ -0,0 +1,145 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: () => import('../layouts/app.vue'),
+      children: [
+        {
+          path: '',
+          redirect: '/home'
+        },
+        {
+          path: 'home',
+          component: () => import('@/pages/home/index.vue')
+        }
+      ]
+    },
+    {
+      path: '/master',
+      component: () => import('../layouts/app.vue'),
+      children: [
+        {
+          path: 'user',
+          component: () => import('@/pages/master/user/index.vue')
+        },
+        {
+          path: 'branch',
+          component: () => import('@/pages/master/branch/index.vue')
+        },
+        {
+          path: 'warehouse',
+          component: () => import('@/pages/master/warehouse/index.vue')
+        },
+        {
+          path: 'allocation',
+          component: () => import('@/pages/master/allocation/index.vue')
+        },
+        {
+          path: 'item',
+          component: () => import('@/pages/master/item/index.vue')
+        }
+      ]
+    },
+    {
+      path: '/purchase',
+      component: () => import('../layouts/app.vue'),
+      children: [
+        {
+          path: 'purchase-request',
+          component: () => import('@/pages/purchase/purchase-request/index.vue')
+        },
+        {
+          path: 'purchase-order',
+          component: () => import('@/pages/purchase/purchase-order/index.vue')
+        },
+        {
+          path: 'down-payment',
+          component: () => import('@/pages/purchase/down-payment/index.vue')
+        },
+        {
+          path: 'receive-order',
+          component: () => import('@/pages/purchase/receive-order/index.vue')
+        },
+        {
+          path: 'invoice',
+          component: () => import('@/pages/purchase/invoice/index.vue')
+        },
+        {
+          path: 'payment-order',
+          component: () => import('@/pages/purchase/payment-order/index.vue')
+        }
+      ]
+    },
+    {
+      path: '/sales',
+      component: () => import('../layouts/app.vue'),
+      children: [
+        {
+          path: 'sales-quotation',
+          component: () => import('@/pages/sales/sales-quotation/index.vue')
+        },
+        {
+          path: 'sales-order',
+          component: () => import('@/pages/sales/sales-order/index.vue')
+        },
+        {
+          path: 'down-payment',
+          component: () => import('@/pages/sales/down-payment/index.vue')
+        },
+        {
+          path: 'delivery-order',
+          component: () => import('@/pages/sales/delivery-order/index.vue')
+        },
+        {
+          path: 'delivery-note',
+          component: () => import('@/pages/sales/delivery-note/index.vue')
+        },
+        {
+          path: 'invoice',
+          component: () => import('@/pages/sales/invoice/index.vue')
+        },
+        {
+          path: 'payment-collection',
+          component: () => import('@/pages/sales/payment-collection/index.vue')
+        }
+      ]
+    },
+    {
+      path: '/accounting',
+      component: () => import('../layouts/app.vue'),
+      children: [
+        {
+          path: 'chart-of-account',
+          component: () => import('@/pages/accounting/chart-of-account/index.vue')
+        },
+        {
+          path: 'cut-off',
+          component: () => import('@/pages/accounting/cut-off/index.vue')
+        }
+      ]
+    },
+    {
+      path: '/manufacture',
+      component: () => import('../layouts/app.vue'),
+      children: [
+        {
+          path: 'formula',
+          component: () => import('@/pages/manufacture/formula/index.vue')
+        },
+        {
+          path: 'machine',
+          component: () => import('@/pages/manufacture/machine/index.vue')
+        },
+        {
+          path: 'process',
+          component: () => import('@/pages/manufacture/process/index.vue')
+        }
+      ]
+    }
+  ]
+})
+
+export default router
