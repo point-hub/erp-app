04fc1b149b420076e52cea71798614a75dee1f42
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-12 12:22:52 +0700

    x

--- src/pages/sales/menu/index.vue
+++ src/pages/sales/menu/index.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
 import AppMenu from '@/components/app-menu.vue'
+import { useAuthStore } from '@/stores/auth.store'
+
+const authStore = useAuthStore()
 
 const breadcrumbs = [
   {
@@ -11,45 +16,61 @@ const breadcrumbs = [
   }
 ]
 
-const menus = [
-  {
-    name: 'Sales Requests',
-    path: '/sales/sales-quotations',
-    icon: 'i-far-file-pen'
-  },
-  {
-    name: 'Sales Orders',
-    path: '/sales/sales-orders',
-    icon: 'i-far-file-pen'
-  },
-  {
-    name: 'Downpayments',
-    path: '/sales/downpayments',
-    icon: 'i-far-file-invoice-dollar'
-  },
-  {
-    name: 'Delivery Orders',
-    path: '/sales/delivery-orders',
-    icon: 'i-far-file-vector'
-  },
-  {
-    name: 'Delivery Notes',
-    path: '/sales/delivery-notes',
-    icon: 'i-far-file-vector'
-  },
-  {
-    name: 'Invoices',
-    path: '/sales/invoices',
-    icon: 'i-far-file-invoice'
-  },
-  {
-    name: 'Payment Collections',
-    path: '/sales/payment-collections',
-    icon: 'i-far-file-invoice-dollar'
+const menus = ref<any[]>([])
+
+onMounted(() => {
+  if (authStore?.permission?.sales?.sales_quotations?.read) {
+    menus.value.push({
+      name: 'Sales Requests',
+      path: '/sales/sales-quotations',
+      icon: 'i-far-file-pen'
+    })
   }
-]
+  if (authStore?.permission?.sales?.sales_orders?.read) {
+    menus.value.push({
+      name: 'Sales Orders',
+      path: '/sales/sales-orders',
+      icon: 'i-far-file-pen'
+    })
+  }
+  if (authStore?.permission?.sales?.down_payments?.read) {
+    menus.value.push({
+      name: 'Down Payments',
+      path: '/sales/down-payments',
+      icon: 'i-far-file-invoice-dollar'
+    })
+  }
+  if (authStore?.permission?.sales?.delivery_orders?.read) {
+    menus.value.push({
+      name: 'Delivery Orders',
+      path: '/sales/delivery-orders',
+      icon: 'i-far-file-vector'
+    })
+  }
+  if (authStore?.permission?.sales?.delivery_notes?.read) {
+    menus.value.push({
+      name: 'Delivery Notes',
+      path: '/sales/delivery-notes',
+      icon: 'i-far-file-vector'
+    })
+  }
+  if (authStore?.permission?.sales?.invoices?.read) {
+    menus.value.push({
+      name: 'Invoices',
+      path: '/sales/invoices',
+      icon: 'i-far-file-invoice'
+    })
+  }
+  if (authStore?.permission?.sales?.payment_orders?.read) {
+    menus.value.push({
+      name: 'Payment Collections',
+      path: '/sales/payment-collections',
+      icon: 'i-far-file-invoice-dollar'
+    })
+  }
+})
 </script>
 
 <template>
-  <app-menu :breadcrumbs="breadcrumbs" :menus="menus" />
+  <app-menu :breadcrumbs="breadcrumbs" v-model:menus="menus" />
 </template>
