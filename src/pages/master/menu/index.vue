3457f0e27e57807632111982e561f689109ddd68
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-12 12:17:39 +0700

    x

--- src/pages/master/menu/index.vue
+++ src/pages/master/menu/index.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
 import AppMenu from '@/components/app-menu.vue'
+import { useAuthStore } from '@/stores/auth.store'
+
+const authStore = useAuthStore()
 
 const breadcrumbs = [
   {
@@ -11,73 +16,100 @@ const breadcrumbs = [
   }
 ]
 
-const menus = [
-  {
-    name: 'Users',
-    path: '/master/users',
-    icon: 'i-far-users'
-  },
-  {
-    name: 'Roles',
-    path: '/master/roles',
-    icon: 'i-far-id-badge'
-  },
-  {
-    name: 'Branches',
-    path: '/master/branches',
-    icon: 'i-far-sitemap'
-  },
-  {
-    name: 'Warehouses',
-    path: '/master/warehouses',
-    icon: 'i-far-warehouse'
-  },
-  {
-    name: 'Chart of Account',
-    path: '/master/chart-of-accounts',
-    icon: 'i-far-book'
-  },
-  {
-    name: 'Allocation Groups',
-    path: '/master/allocation-groups',
-    icon: 'i-far-notes'
-  },
-  {
-    name: 'Allocations',
-    path: '/master/allocations',
-    icon: 'i-far-note'
-  },
-  {
-    name: 'Supplier Groups',
-    path: '/master/supplier-groups',
-    icon: 'i-far-files'
-  },
-  {
-    name: 'Suppliers',
-    path: '/master/suppliers',
-    icon: 'i-far-user-group-simple'
-  },
-  {
-    name: 'Customer Groups',
-    path: '/master/customer-groups',
-    icon: 'i-far-files'
-  },
-  {
-    name: 'Customers',
-    path: '/master/customers',
-    icon: 'i-far-user-group-crown'
-  },
-  {
-    name: 'Item Categories',
-    path: '/master/item-categories',
-    icon: 'i-far-tags'
-  },
-  {
-    name: 'Items',
-    path: '/master/items',
-    icon: 'i-far-boxes-stacked'
+const menus = ref<any[]>([])
+
+onMounted(() => {
+  if (authStore?.permission?.master?.users?.read) {
+    menus.value.push({
+      name: 'Users',
+      path: '/master/users',
+      icon: 'i-far-users'
+    })
   }
-]
+  if (authStore?.permission?.master?.roles?.read) {
+    menus.value.push({
+      name: 'Roles',
+      path: '/master/roles',
+      icon: 'i-far-id-badge'
+    })
+  }
+  if (authStore?.permission?.master?.branches?.read) {
+    menus.value.push({
+      name: 'Branches',
+      path: '/master/branches',
+      icon: 'i-far-sitemap'
+    })
+  }
+  if (authStore?.permission?.master?.warehouses?.read) {
+    menus.value.push({
+      name: 'Warehouses',
+      path: '/master/warehouses',
+      icon: 'i-far-warehouse'
+    })
+  }
+  if (authStore?.permission?.master?.chart_of_accounts?.read) {
+    menus.value.push({
+      name: 'Chart of Account',
+      path: '/master/chart-of-accounts',
+      icon: 'i-far-book'
+    })
+  }
+  if (authStore?.permission?.master?.setting_journals?.read) {
+    menus.value.push({
+      name: 'Setting Journals',
+      path: '/master/setting-journals',
+      icon: 'i-far-notebook'
+    })
+  }
+  if (authStore?.permission?.master?.allocations?.read) {
+    menus.value.push({
+      name: 'Allocation Groups',
+      path: '/master/allocation-groups',
+      icon: 'i-far-notes'
+    })
+    menus.value.push({
+      name: 'Allocations',
+      path: '/master/allocations',
+      icon: 'i-far-note'
+    })
+  }
+  if (authStore?.permission?.master?.suppliers?.read) {
+    menus.value.push({
+      name: 'Supplier Groups',
+      path: '/master/supplier-groups',
+      icon: 'i-far-files'
+    })
+    menus.value.push({
+      name: 'Suppliers',
+      path: '/master/suppliers',
+      icon: 'i-far-user-group-simple'
+    })
+  }
+  if (authStore?.permission?.master?.customers?.read) {
+    menus.value.push({
+      name: 'Customer Groups',
+      path: '/master/customer-groups',
+      icon: 'i-far-files'
+    })
+    menus.value.push({
+      name: 'Customers',
+      path: '/master/customers',
+      icon: 'i-far-user-group-crown'
+    })
+  }
+  if (authStore?.permission?.master?.items?.read) {
+    menus.value.push({
+      name: 'Item Categories',
+      path: '/master/item-categories',
+      icon: 'i-far-tags'
+    })
+    menus.value.push({
+      name: 'Items',
+      path: '/master/items',
+      icon: 'i-far-boxes-stacked'
+    })
+  }
+})
 </script>
 
 <template>
