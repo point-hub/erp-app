d30f9a08a9db431a61ef0b55fd20b03b7d2696b8
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-12 12:25:57 +0700

    x

--- src/pages/manufacture/menu/index.vue
+++ src/pages/manufacture/menu/index.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
 import AppMenu from '@/components/app-menu.vue'
+import { useAuthStore } from '@/stores/auth.store'
+
+const authStore = useAuthStore()
 
 const breadcrumbs = [
   {
@@ -11,25 +16,40 @@ const breadcrumbs = [
   }
 ]
 
-const menus = [
-  {
-    name: 'Machines',
-    path: '/manufacture/machines',
-    icon: 'i-far-conveyor-belt'
-  },
-  {
-    name: 'Formulas',
-    path: '/manufacture/formulas',
-    icon: 'i-far-file-check'
-  },
-  {
-    name: 'Processes',
-    path: '/manufacture/processes',
-    icon: 'i-far-arrow-progress'
+const menus = ref<any[]>([])
+
+onMounted(() => {
+  if (authStore?.permission?.manufacture?.machines?.read) {
+    menus.value.push({
+      name: 'Machines',
+      path: '/manufacture/machines',
+      icon: 'i-far-conveyor-belt'
+    })
   }
-]
+  if (authStore?.permission?.manufacture?.formulas?.read) {
+    menus.value.push({
+      name: 'Formulas',
+      path: '/manufacture/formulas',
+      icon: 'i-far-file-check'
+    })
+  }
+  if (authStore?.permission?.manufacture?.processes?.read) {
+    menus.value.push({
+      name: 'Processes',
+      path: '/manufacture/processes',
+      icon: 'i-far-arrow-progress'
+    })
+  }
+  if (authStore?.permission?.manufacture?.processing?.read) {
+    menus.value.push({
+      name: 'Processing',
+      path: '/manufacture/processing',
+      icon: 'i-far-conveyor-belt-boxes'
+    })
+  }
+})
 </script>
 
 <template>
-  <app-menu :breadcrumbs="breadcrumbs" :menus="menus" />
+  <app-menu :breadcrumbs="breadcrumbs" v-model:menus="menus" />
 </template>
