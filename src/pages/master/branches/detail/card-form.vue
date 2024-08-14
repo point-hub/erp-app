d4b8bd0ada2a1d6d97371ab96326b8a8fffbcac2
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-15 01:10:20 +0700

    x

--- src/pages/master/branches/detail/card-form.vue
+++ src/pages/master/branches/detail/card-form.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import type { IFormError } from './form'
-
 const code = defineModel<string>('code')
 const name = defineModel<string>('name')
 const address = defineModel<string>('address')
 const phone = defineModel<string>('phone')
-const errors = defineModel<IFormError>('errors')
 </script>
 
 <template>
@@ -13,10 +10,10 @@ const errors = defineModel<IFormError>('errors')
     <template #header>Branches</template>
 
     <div class="flex flex-col gap-4 mt-5">
-      <base-input disabled v-model="code" label="Code" :errors="errors?.code" />
-      <base-input disabled v-model="name" label="Name" :errors="errors?.name" />
-      <base-input disabled v-model="address" label="Address" :errors="errors?.address" />
-      <base-input disabled v-model="phone" label="Phone" :errors="errors?.phone" />
+      <base-input disabled v-model="code" label="Code" />
+      <base-input disabled v-model="name" label="Name" />
+      <base-input disabled v-model="address" label="Address" />
+      <base-input disabled v-model="phone" label="Phone" />
     </div>
   </base-card>
 </template>
