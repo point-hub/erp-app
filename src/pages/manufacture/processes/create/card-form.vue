693e448fadc24d0fe76c4c26a381835de2c835d2
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-15 09:48:25 +0700

    x

--- src/pages/manufacture/processes/create/card-form.vue
+++ src/pages/manufacture/processes/create/card-form.vue
@@ -11,9 +11,8 @@ const helpers = ref(['This code is used to generate item code based on the selec
 
 watch(code, () => {
   code.value = code.value?.toUpperCase() ?? ''
-  if (code.value?.length > 4) {
-    code.value = code.value.substring(0, 4)
-    helpers.value[1] = `The code is too long. Max length is 4.`
+  if (code.value?.length === 4) {
+    helpers.value[1] = `The code max length is 4.`
   }
 })
 </script>
@@ -23,7 +22,14 @@ watch(code, () => {
     <template #header>Processes</template>
 
     <div class="flex flex-col gap-4 mt-5">
-      <base-input required v-model="code" label="Code" :errors="errors?.code" :helpers="helpers" />
+      <base-input
+        required
+        v-model="code"
+        label="Code"
+        :errors="errors?.code"
+        :helpers="helpers"
+        :maxlength="4"
+      />
       <base-input required v-model="name" label="Name" :errors="errors?.name" />
       <base-textarea v-model="notes" label="Notes" :errors="errors?.notes" :minHeight="128" />
     </div>
--- src/pages/manufacture/processes/edit/card-breadcrumbs.vue
+++ src/pages/manufacture/processes/edit/card-breadcrumbs.vue
@@ -11,7 +11,7 @@ const breadcrumbs: IBreadcrumb[] = [
     path: '/menu'
   },
   {
-    name: 'Master',
+    name: 'Manufacture',
     path: '/manufacture'
   },
   {
