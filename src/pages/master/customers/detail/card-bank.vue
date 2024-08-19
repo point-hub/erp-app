5a178cca8d1498cf53c21e7d8747cda680d1db77
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-19 11:14:20 +0700

    x

--- src/pages/master/customers/detail/card-bank.vue
+++ src/pages/master/customers/detail/card-bank.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import type { IFormError } from './form'
-
 const branch = defineModel<string>('branch')
 const name = defineModel<string>('name')
 const account_name = defineModel<string>('account_name')
 const account_number = defineModel<string>('account_number')
-const errors = defineModel<IFormError>('errors')
 </script>
 
 <template>
@@ -13,14 +10,10 @@ const errors = defineModel<IFormError>('errors')
     <template #header>Bank Info</template>
 
     <div class="flex flex-col gap-4 mt-5">
-      <base-input v-model="name" label="Name" :errors="errors?.bank_name" />
-      <base-input v-model="branch" label="Branch" :errors="errors?.bank_branch" />
-      <base-input v-model="account_name" label="Account Name" :errors="errors?.bank_address" />
-      <base-input
-        v-model="account_number"
-        label="Account Number"
-        :errors="errors?.bank_account_number"
-      />
+      <base-input v-model="name" label="Name" />
+      <base-input v-model="branch" label="Branch" />
+      <base-input v-model="account_name" label="Account Name" />
+      <base-input v-model="account_number" label="Account Number" />
     </div>
   </base-card>
 </template>
